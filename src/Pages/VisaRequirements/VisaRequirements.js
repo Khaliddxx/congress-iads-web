1cede515f97ce366a24002be7109f19162430aad
elshabshiri@aucegypt.edu <moelshabshiri>
2022-04-21 12:15:44 +0200

    updates

---
+++ src/Pages/VisaRequirements/VisaRequirements.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./VisaRequirements.scss";
+import visaimg from "../../Assets/Images/visa.svg";
+
+function VisaRequirements() {
+  return (
+    <>
+      <div className="visaPage container">
+        <div className="visaTitle">
+          <p className="visaTitleText">Visa</p>
+          <p className="hongKong">Requirements</p>
+        </div>
+        <div className="visaimg"></div>
+      </div>
+    </>
+  );
+}
+
+export default VisaRequirements;
